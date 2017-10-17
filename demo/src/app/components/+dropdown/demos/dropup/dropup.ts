2077d81b84ecd0299c1e2f12cd85e61f3d8e5485
david weitzman <david@chorus.ai>
2017-10-17 14:24:00 +0300

    dist

--- demo/src/app/components/+dropdown/demos/dropup/dropup.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'demo-dropup',
-  templateUrl: './dropup.html'
-})
-export class DemoDropupComponent {
-  isDropup = true;
-}
--- src/datepicker/date-formatter.ts
+++
@@ -1,7 +0,0 @@
-import { formatDate } from '../bs-moment/format';
-
-export class DateFormatter {
-  format(date: Date, format: string, locale: string): string {
-    return formatDate(date, format, locale);
-  }
-}
